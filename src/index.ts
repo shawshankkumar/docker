9061a1719cb4b236abf03b02f37b1357a0f73e4f
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-06-16 23:32:03 +0530

    ✨ Finish the password generation route

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,20 @@ const server = fastify();
 
 server.get("/", async (req, reply) => {
   try {
-    reply.status(200);
-    return { password: nanoid(), success: true };
+    console.log("New password generated");
+    reply.type("application/json").status(200);
+    return {
+      message: "successfully generated a random password",
+      password: nanoid(),
+      success: true,
+    };
   } catch (err) {
     reply.status(500);
     return { success: false, message: "failed to create a secure password" };
   }
 });
 
-server.listen(3000, (err, address) => {
+server.listen(3000, "0.0.0.0", (err, address) => {
   if (err) {
     console.error(err);
   }
